f65de9a3a1532e46dc66cfcbd844d2b5670c5ce4
Danilo Vitoriano <dvitoriano@uolinc.com>
2019-11-12 14:41:07 -0300

    separando os componentes em slots

--- src/components/Tela.vue
+++ src/components/Tela.vue
@@ -12,14 +12,9 @@
     <img :src="foto.url" :alt="foto.alt" />
     <ul class="lista-fotos">
       <li v-for="post of posts" v-bind:key="post.id" class="lista-fotos-item">
-        <!-- inicio painel -->
-        <div class="painel">
-          <h2 class="painel-titulo">{{post.first_name + ' ' + post.last_name}}</h2>
-          <div class="painel-corpo">
-            <img class="imagem-responsiva" :src="post.avatar" :title="post.email" />
-          </div>
-        </div>
-        <!-- fim painel -->
+        <meu-painel :titulo="post.first_name + ' ' + post.last_name">
+          <img class="imagem-responsiva" :src="post.avatar" :title="post.email" />
+        </meu-painel>
       </li>
     </ul>
   </div>
@@ -27,7 +22,12 @@
 
 <script>
 import axios from "axios";
+import Painel from './shared/painel/Painel'
+
 export default {
+  components: {
+    'meu-painel': Painel
+  },
   name: "Tela",
   created() {
     axios
@@ -101,32 +101,5 @@ export default {
   display: inline-block;
 }
 
-/* estilo do painel */
 
-.imagem-responsiva {
-    width: 100%;
-  }
-
-.painel {
-  padding: 0 auto;
-  border: solid 1px grey;
-  display: inline-block;
-  margin: 0 10px;
-  box-shadow: 2px 2px 10px grey;
-  width: 150px;
-  height: 100%;
-  vertical-align: top;
-  text-align: center;
-}
-
-.painel .painel-titulo {
-  font-size: 14px;
-  text-align: center;
-  border-bottom: solid 1px grey;
-  background: mediumspringgreen;
-  margin: 0;
-  padding: 10px;
-  text-transform: uppercase;
-
-}
 </style>
---
+++ src/components/shared/painel/Painel.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="painel">
+    <h2 class="painel-titulo">{{titulo}}</h2>
+    <slot class="painel-corpo">
+      
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['titulo']
+};
+</script>
+
+<style scoped>
+/* estilo do painel */
+
+.imagem-responsiva {
+  width: 100%;
+}
+
+.painel {
+  padding: 0 auto;
+  border: solid 1px grey;
+  display: inline-block;
+  margin: 0 10px;
+  box-shadow: 2px 2px 10px grey;
+  width: 150px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.painel .painel-titulo {
+  font-size: 14px;
+  text-align: center;
+  border-bottom: solid 1px grey;
+  background: mediumspringgreen;
+  margin: 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
+</style>
